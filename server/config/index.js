cc1505a0eedaf493916b4c5a6e2b8e761b7e6f8e
rivalcoba <ivan.rivalcoba@gmail.com>
2020-07-01 20:12:11 -0500

    Sending Email confirmation

--- server/config/index.js
+++ server/config/index.js
@@ -7,4 +7,5 @@ dotenv.config();
 export default {
     databaseUrl:
         process.env.DATABASE_URL || 'mongodb://localhost:27017/mevnmongo',
+    url: process.env.APP_URL || 'http://localhost:3000'
 };
--- server/models/Users.js
+++ server/models/Users.js
@@ -1,9 +1,13 @@
+// importing configuration
+import config from '@config'
 // Importing mongoose
 import mongoose from 'mongoose';
 // Importing library to encript passwords
 import Bcrypt from 'bcryptjs';
 // Generates Random String
 import randomstring from 'randomstring';
+// Importing mail package
+import Mail from '@fullstackjs/mail';
 
 const UserSchema = new mongoose.Schema({
     name: String,
@@ -23,5 +27,17 @@ UserSchema.pre('save', function(){
     this.emailConfirmCode = randomstring.generate(72);
     this.createdAt = new Date();
 });
+
+UserSchema.post('save',async function(){
+    console.log(`> CODE: ${config.url}/auth/emails/confirm/${this.emailConfirmCode}`);
+    await new Mail('confirm-account')
+    .to(this.email, this.name)
+    .subject('Please Confirm your account')
+    .data({
+        name: this.name,
+        url: `${config.url}/auth/emails/confirm/${this.emailConfirmCode}`
+    })
+    .send();
+})
 
 export default mongoose.model('User', UserSchema);
