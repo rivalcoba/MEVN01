73bd5b8a719c604e98270d266ffe8443c09d26d9
rivalcoba <ivan.rivalcoba@gmail.com>
2020-07-14 17:45:02 -0500

    USER LOGIN CLIENT SIDE

--- client/pages/Login.vue
+++ client/pages/Login.vue
@@ -1,3 +1,93 @@
 <template>
-    <h1>Login</h1>
+    <div class="container my-16 w-full h-12 mx-auto">
+        <div class="max-w-xs mx-auto h-12">
+            <h1 class="text-lg text-center text-gold">Login</h1>
+            <div class="w-full bg-white shadow mt-5 rounded-sm p-8">
+                <ValidationObserver ref="form">
+                    
+                    <text-input
+                    name="email"
+                    rules="required|email"
+                    :value="model.email"
+                    v-model="model.email"
+                    placeholder="Enter Your Email"></text-input>
+
+                    <text-input
+                    type="password"
+                    name="password"
+                    rules="required|min:6"
+                    :value="model.password"
+                    v-model="model.password"
+                    placeholder="Enter Your Password"></text-input>
+
+                    <btn
+                    label="Sign In"
+                    :disabled="loading"
+                    :loading="loading"
+                    @click="login"
+                    />
+
+                    <!-- <button @click="register" class="w-full mt-3 py-3 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-light">Sing Up</button> -->
+                </ValidationObserver>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+// Importing form Validators
+import {ValidationProvider, ValidationObserver, validate} from 'vee-validate';
+// Importing client requests or client actions
+import {POST_LOGIN, SET_AUTH} from '@client/store/auth/actions'
+
+export default {
+    data: ()=>({
+        loading: false,
+        model: {
+            email:'',
+            password:''
+        }
+    }),
+    components: {
+        ValidationProvider,
+        ValidationObserver
+    },
+    methods: {
+    login(){
+      this.$refs.form.validate().then(isValid=>{
+          if(!isValid){
+              return;
+          }
+          // Change loading state
+          this.toggleLoading();
+          // An action returns a promise
+          this.$store.dispatch(POST_LOGIN, this.model)
+          .then(response=>{
+              //alert("Finished!!!");
+              this.toggleLoading();
+              // Persist on local storage
+              localStorage.setItem('auth', JSON.stringify(response.data))
+              // Commit auth succesfull
+              this.$store.commit(SET_AUTH, response.data)
+              // Redirecting user to the home page
+              this.$router.push('/');
+          })
+          .catch(error => {
+              this.toggleLoading()
+              let backendErrors = {};
+              //console.log(">" + Object.keys(error.response.data))
+              Object.keys(error.response.data).forEach(field => {
+                  // Adding errors from the backend to the observer
+                  backendErrors[field] = [error.response.data[field]]
+              })
+              //console.log("> " + Object.keys(backendErrors))
+              this.$refs.form.setErrors(backendErrors)
+          });
+      });
+    },
+    toggleLoading(){
+        this.loading = !this.loading;
+    }
+  }
+}
+</script>
--- client/store/auth/actions.js
+++ client/store/auth/actions.js
@@ -5,7 +5,10 @@ export const SET_AUTH = 'SET_AUTH';
 
 export const POST_REGISTER = 'POST_REGISTER';
 
+export const POST_LOGIN = 'POST_LOGIN'
+
 export default {
-    [POST_REGISTER]: (context, data) => client.post('auth/register', data),
     //[POST_REGISTER]:(context, data)=> client.post('auth/register')
+    [POST_REGISTER]: (context, data) => client.post('auth/register', data),
+    [POST_LOGIN]: (context, data)=> client.post('auth/login', data)
 };
